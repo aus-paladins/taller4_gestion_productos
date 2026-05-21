8dc6e810720cfd9c9fbd2eed2fa907813dcdf1c6
Andre Busnelli <andrebusnelli@gmail.com>
2026-05-20 23:54:57 -0300

    start.spring.io

---
+++ gestorback/src/main/java/aus/t4/paladins/gestorback/GestorbackApplication.java
@@ -0,0 +1,13 @@
+package aus.t4.paladins.gestorback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorbackApplication.class, args);
+	}
+
+}
