7b30858b1759cf963d25c547013e43f24e0900fd
Andre Busnelli <andrebusnelli@gmail.com>
2026-05-26 01:11:06 -0300

    feat: se implementa modelo de entidad JPA

---
+++ gestorback/src/main/java/aus/t4/paladins/gestorback/model/Categoria.java
@@ -0,0 +1,30 @@
+package aus.t4.paladins.gestorback.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor // requerido por JPA y Builder
+@Builder
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+
+    @OneToMany(mappedBy = "categoria", 
+        cascade = CascadeType.ALL, // actuar s/padre entero repica en el hijo
+        orphanRemoval = true,       // al desconectar un hijo no queda huérfano
+        fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<Producto> productos = new ArrayList<>();
+
+}
